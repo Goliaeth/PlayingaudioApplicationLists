23dabd41de412ab505b518b0fa3e7dbdd6b06dac
Goliaeth <75118259+Goliaeth@users.noreply.github.com>
2022-09-13 21:41:26 +0500

    forgotten initial commit

---
+++ app/src/main/java/com/nick/playingaudioapplicationlists/CustomAdapter.kt
@@ -0,0 +1,49 @@
+package com.nick.playingaudioapplicationlists
+
+import android.content.Context
+import android.media.MediaPlayer
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+
+class CustomAdapter(var arrayList: ArrayList<Data>) : BaseAdapter() {
+    override fun getCount() = arrayList.size
+
+    override fun getItem(position: Int) = arrayList[position]
+
+    override fun getItemId(position: Int) = position.toLong()
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+
+        val context = parent?.context
+        val inflator: LayoutInflater =
+            context?.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+        val rowView: View = inflator.inflate(R.layout.item_list, parent, false)
+
+        val item = arrayList[position]
+
+        val numberTextView = rowView.findViewById<TextView>(R.id.number_text_view)
+        numberTextView.text = item.number
+
+        val audioImageView = rowView.findViewById<ImageView>(R.id.audio_image_view)
+        audioImageView.setOnClickListener {
+
+            val mediaPlayer =
+                MediaPlayer.create(
+                    context,
+                    context.resources.getIdentifier(
+                        item.audioFileName, "raw",
+                        context.packageName
+                    )
+                )
+            mediaPlayer.start()
+
+        }
+
+        return rowView
+    }
+}
---
+++ app/src/main/java/com/nick/playingaudioapplicationlists/Data.kt
@@ -0,0 +1,3 @@
+package com.nick.playingaudioapplicationlists
+
+data class Data(val number : String, val audioFileName : String)
